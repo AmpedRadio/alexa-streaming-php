35a6045a6c73e1fc753b8bc1aafa59ac8f926745
Bill Condo <bill@billcondo.com>
2019-09-25 19:35:18 -0400

    DocBlock Update

--- src/AlexaStreaming.php
+++ src/AlexaStreaming.php
@@ -29,7 +29,7 @@ class AlexaStreaming
     /** @var RequestValidator $validator */
     public $validator;
 
-    /** @var $is_valid_request */
+    /** @var bool|null $is_valid_request */
     public $is_valid_request;
 
     /**
